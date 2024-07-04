a9d31b6d90fb15b4904ba8ed624f623091be8ea2
ArthurAAM <arthur.azevedo.maia@gmail.com>
2024-07-04 16:54:02 -0300

    add: routes for execution and execution events

--- src/resources/execution.py
+++ src/resources/execution.py
@@ -1,11 +1,64 @@
 import falcon
 import logging
 
-from controllers.execution_event import ExecutionEventController
+from controllers.execution import ExecutionController
 from exceptions import ClientException
 
 class ExecutionResource(object):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    # TODO: Implement Routes
+    def on_get_list(self, req, resp):
+        self.logger.info(f'Retrieving all executions')
+        try:
+            execution_controller = ExecutionController(req.context.db_session)
+            response = execution_controller.retrieve_all_executions()
+        except ClientException as x:
+            resp.media = x.get()
+            resp.status = x.http_status()
+            return
+        
+        resp.media = response
+        resp.status = falcon.HTTP_200
+
+    def on_get_by_key(self, req, resp, execution_key):
+        self.logger.info(f'Retrieving execution for execution key {execution_key}')
+        
+        try:
+            execution_controller = ExecutionController(req.context.db_session)
+            response = execution_controller.retrieve_execution_by_key(execution_key=execution_key)
+        except ClientException as x:
+            resp.media = x.get()
+            resp.status = x.http_status()
+            return
+        
+        resp.media = response
+        resp.status = falcon.HTTP_200
+
+    def on_delete_by_key(self, req, resp, execution_key):
+        self.logger.info(f'Deleting execution with execution key {execution_key}')
+        
+        try:
+            execution_controller = ExecutionController(req.context.db_session)
+            response = execution_controller.delete_execution_by_key(execution_key=execution_key)
+        except ClientException as x:
+            resp.media = x.get()
+            resp.status = x.http_status()
+            return
+        
+        resp.media = response
+        resp.status = falcon.HTTP_200
+        
+    def on_post(self, req, resp):
+        self.logger.info(f'Posting execution event {req.media}')
+
+        try:
+            execution_controller = ExecutionController(req.context.db_session)
+            response = execution_controller.create_new_execution(req.media)
+        except ClientException as x:
+            resp.media = x.get()
+            resp.status = x.http_status()
+            return
+        
+        resp.media = response
+        resp.status = falcon.HTTP_200
--- src/resources/execution_event.py
+++ src/resources/execution_event.py
@@ -8,4 +8,57 @@ class ExecutionEventResource(object):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    # TODO: Implement Routes
+    def on_get_list(self, req, resp):
+        self.logger.info(f'Retrieving all execution events')
+        try:
+            execution_event_controller = ExecutionEventController(req.context.db_session)
+            response = execution_event_controller.retrieve_all_execution_events()
+        except ClientException as x:
+            resp.media = x.get()
+            resp.status = x.http_status()
+            return
+        
+        resp.media = response
+        resp.status = falcon.HTTP_200
+
+    def on_get_by_key(self, req, resp, execution_event_key):
+        self.logger.info(f'Retrieving execution event for execution_event key {execution_event_key}')
+        
+        try:
+            execution_event_controller = ExecutionEventController(req.context.db_session)
+            response = execution_event_controller.retrieve_execution_event_by_key(execution_event_key=execution_event_key)
+        except ClientException as x:
+            resp.media = x.get()
+            resp.status = x.http_status()
+            return
+        
+        resp.media = response
+        resp.status = falcon.HTTP_200
+
+    def on_delete_by_key(self, req, resp, execution_event_key):
+        self.logger.info(f'Deleting execution event with execution event key {execution_event_key}')
+        
+        try:
+            execution_event_controller = ExecutionEventController(req.context.db_session)
+            response = execution_event_controller.delete_execution_event_by_key(execution_event_key=execution_event_key)
+        except ClientException as x:
+            resp.media = x.get()
+            resp.status = x.http_status()
+            return
+        
+        resp.media = response
+        resp.status = falcon.HTTP_200
+        
+    def on_post(self, req, resp):
+        self.logger.info(f'Posting execution event {req.media}')
+
+        try:
+            execution_event_controller = ExecutionEventController(req.context.db_session)
+            response = execution_event_controller.create_new_execution_event(req.media)
+        except ClientException as x:
+            resp.media = x.get()
+            resp.status = x.http_status()
+            return
+        
+        resp.media = response
+        resp.status = falcon.HTTP_200
