076e42b5e681487cd83ec566c3dfe70d5a809ff8
ArthurAAM <arthur.azevedo.maia@gmail.com>
2024-07-11 17:57:06 -0300

    Saving changes

--- src/models/execution.py
+++ src/models/execution.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String ,ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Time, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import JSONB
 from models.base import Base
@@ -9,10 +9,9 @@ class Execution(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     execution_key = Column(String)
-    created_at = Column(String)
+    created_at = Column(DateTime)
     execution_data = Column(JSONB)
     routine_id = Column(Integer, ForeignKey('routine.id'))
-    trigger_at = Column(String)
+    trigger_at = Column(Time)
 
-    routine = relationship('Routine', back_populates='executions')
-    events = relationship('ExecutionEvent', back_populates='execution')
+    routine = relationship('Routine')
