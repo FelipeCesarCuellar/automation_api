daac7cdf99fb3378396e2d2d89feffddd7400e4a
ArthurAAM <arthur.azevedo.maia@gmail.com>
2024-07-11 18:49:28 -0300

    fixed database

--- database/database.sql
+++ database/database.sql
@@ -9,18 +9,18 @@ CREATE TABLE routine (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     routine_key CHAR(36) NOT NULL,
-    created_at DATETIME NOT NULL,
-    deactivated_on DATETIME
+    created_at TIMESTAMP NOT NULL,
+    deactivated_on TIMESTAMP
 );
 
 CREATE TABLE execution (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     execution_key CHAR(36) NOT NULL,
-    created_at DATETIME NOT NULL,
+    created_at TIMESTAMP NOT NULL,
     execution_data JSONB NOT NULL,
     routine_id INTEGER,
-    trigger_at DATETIME,
+    trigger_at TIMESTAMP,
     UNIQUE (execution_key),
     FOREIGN KEY (routine_id) REFERENCES routine(id)
 );
@@ -29,9 +29,9 @@ CREATE TABLE execution_instance(
     id SERIAL PRIMARY KEY,
     execution_instance_key CHAR(36) NOT NULL,
     execution_id INTEGER,
-    created_at DATETIME NOT NULL,
-    trigger_at DATETIME NOT NULL,
-    triggered_at DATETIME,
+    created_at TIMESTAMP NOT NULL,
+    trigger_at TIMESTAMP NOT NULL,
+    triggered_at TIMESTAMP,
     UNIQUE (execution_instance_key),
     FOREIGN KEY (execution_id) REFERENCES execution(id)
 );
@@ -40,8 +40,8 @@ CREATE TABLE execution_instance_event (
     id SERIAL PRIMARY KEY,
     execution_instance_event_key CHAR(36) NOT NULL,
     execution_instance_id INTEGER,
-    created_at DATETIME NOT NULL,
-    executed_at DATETIME,
+    created_at TIMESTAMP NOT NULL,
+    executed_at TIMESTAMP,
     UNIQUE (execution_instance_event_key),
     FOREIGN KEY (execution_instance_id) REFERENCES execution_instance(id)
 );
